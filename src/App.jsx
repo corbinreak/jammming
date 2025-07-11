a1399c8d6eca3a2312c3c68eb047e99cc14450af
Corbin Reak <corbinreak@gmail.com>
2025-07-11 13:53:13 -0600

    Revert "Second commit; Added jsx component files and basic handler functions"
    This reverts commit a32316d805be3e876575406b3b2f1bf746444cb0.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
 
@@ -6,18 +9,14 @@ function App() {
     return <div>Search Bar Here</div>; //mock function to take place and define SearchBar.
   }
 
-  
-
-  
-
-  return (
+   return (
     <div>
       <h1>Jammming</h1>
       <SearchBar />
-      <SearchResults />
-      <Playlist />
     </div>
   );
 }
 
-export default App;
+  
+
+ export default SearchBar;
